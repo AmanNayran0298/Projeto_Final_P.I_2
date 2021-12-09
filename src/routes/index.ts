cb986468fd01191c975fa3b568b0a46fa3ddd530
Guilherme Daniel <gui.dani13@gmail.com>
2021-12-09 16:18:29 -0300

    rota pedido-orcamento corrigida

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import pedidoOrcamentoRoute from './PedidoOrcamentoRoute'
 // Importar rotas específicas
 
 const router = Router()
 
 // Adicionar rotas
-router.use('/pedido-orcamento', PedidoOrcaomentoRoutes)
+router.use('/pedido-orcamento', pedidoOrcamentoRoute)
 
 export default router
