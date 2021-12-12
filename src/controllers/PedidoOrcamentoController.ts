928d52bf429b27f3dc83a6c50ab44cafed26cc8e
Guilherme Daniel <gui.dani13@gmail.com>
2021-12-12 10:57:14 -0300

    update status

--- src/controllers/PedidoOrcamentoController.ts
+++ src/controllers/PedidoOrcamentoController.ts
@@ -9,25 +9,35 @@ export class PedidoOrcamentoController {
     // criar métodos get, create, etc...
     // executar uma query/command passando como parametro o a instancia do repositorio criada
 
-    public async getPedido (req: Request, res: Response): Promise<Response>{
+    public async getPedido(req: Request, res: Response): Promise<Response> {
         // const data = MongoPedidoOrcamentoRepository.getAll()
         // console.log(data);
-        
+
         return res.status(200).json({ 'ok': 'ok' })
     }
 
-    // public async create (req: Request, res: Response): Promise<Response> => {
-    //     const {
-    //         numeroPedido,
-    //         marcaVeiculo,
-    //         modeloVeiculo,
-    //         quilometragem,
-    //         servicos,
-    //         totalServico,
-    //         dataPedidoOrcamento
-    //     } = req.body
+    public async createPedido(req: Request, res: Response): Promise<Response> {
+        const {
+            numeroPedido,
+            marcaVeiculo,
+            modeloVeiculo,
+            quilometragem,
+            servicos,
+            totalServico,
+            dataPedidoOrcamento
+        } = req.body
+        if (!numeroPedido || !marcaVeiculo || !modeloVeiculo || !quilometragem || !servicos || !totalServico || !dataPedidoOrcamento){
+            return res.status(400).json( { msg: "not ok"})
+        }
+        console.log(numeroPedido,
+            marcaVeiculo,
+            modeloVeiculo,
+            quilometragem,
+            servicos,
+            totalServico,
+            dataPedidoOrcamento);
 
 
-    //     return res.status(201).json({ numeroPedido, marcaVeiculo, modeloVeiculo, quilometragem, servicos, totalServico, dataPedidoOrcamento })
-    // }
+        return res.status(201).json({ numeroPedido, marcaVeiculo, modeloVeiculo, quilometragem, servicos, totalServico, dataPedidoOrcamento })
+    }
 }
--- src/routes/PedidoOrcamentoRoute.ts
+++ src/routes/PedidoOrcamentoRoute.ts
@@ -6,5 +6,6 @@ const router = Router()
 const pedidoOrcamentoController = new PedidoOrcamentoController()
 
 router.get('/', pedidoOrcamentoController.getPedido)
+router.post('/', pedidoOrcamentoController.createPedido)
 
 export default router;
