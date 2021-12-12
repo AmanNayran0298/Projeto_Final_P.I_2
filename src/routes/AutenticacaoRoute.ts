dee22f5959b60f20127ac68f0a3a3db589f6b097
Guilherme Daniel <gui.dani13@gmail.com>
2021-12-12 14:31:29 -0300

    formatação

--- src/routes/AutenticacaoRoute.ts
+++ src/routes/AutenticacaoRoute.ts
@@ -1,15 +1,30 @@
-import { Router } from "express";
+import { NextFunction, Router, Request, Response } from "express";
 import { AutenticacaoController } from "../controllers/AutenticacaoController";
+const jwt = require('jsonwebtoken')
 
 const router = Router()
+
+const checkToken = function(req: Request, res: Response, next: NextFunction){
+    const authHeader = req.header['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token)return res.status(401).json({msg: 'Acesso negado'});
+    try {
+        const secret = process.env.SECRET
+        jwt.verify(token, secret)
+        next()    
+    } catch (error) {
+        return res.status(404).json({msg: 'Token invalido'})
+    }
+}
+
+router.use(checkToken)
 
 // instancia da classe de autenticação
 const autenticacaoController = new AutenticacaoController();
 
 // #TODO: corrigir middlware
 router.get('/user/:id',checkToken, autenticacaoController.getUserId)
-
-// middleware
 
 // Register User
 router.post('/auth/register', autenticacaoController.registerUser)
